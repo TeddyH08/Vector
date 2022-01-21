f0fc69150393ade70c1588d7faabfaff534c7dde
yanis.pirlet <yanis.pirlet@gmail.com>
2022-01-21 14:44:47 +0100

    img

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+const videos = document.querySelectorAll(".video");
+
+for (const video of videos) {
+    video.addEventListener('mouseover', function() { video.play() }, false);
+    video.addEventListener('mouseout', function() { video.pause() }, false);
+}
